d573eee11660557d8e976fad8db8aedd7e12383f
alex110352@gmail.com <alex110352@gmail.com>
2023-07-17 15:00:34 +0800

    init

---
+++ src/main/java/com/example/pesha/PeshaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pesha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeshaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pesha/PeshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pesha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
